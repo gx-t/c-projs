dce2663c757cadcb27be0154beaba90e4b72717e
Artashes Shahinyan <artashes@vbox-xubuntu.(none)>
2014-04-11 16:46:19 +0400

    Added group aliases, auto doc generation, link to open html and to download doc

--- report-cgi/report.c
+++ report-cgi/report.c
@@ -15,9 +15,38 @@ static int r_weeknum()
   return atoi(buff);
 }
 
+static const char* r_grpname(const char* key)
+{
+  const char* ss =
+  "ya\0Manufacturing Test Group\0"
+  "mdg\0MASIS Design Group\0"
+  "rtl\0RTL Team\0"
+  "qa\0QA Team\0";
+  const char* p1 = ss;
+  while(*p1)
+  {
+    const char* p2 = key;
+    while(*p1 && *p2 == *p1) p1++, p2++;
+    if(*p1 == *p2)
+    {
+      const char* value = ++p1;
+      return value;
+    }
+    while(*p1) p1++; p1++;
+    while(*p1) p1++; p1++;
+  }
+  return 0;
+}
+
 static void r_form(char* arg)
 {
   if(!*arg) return;
+  const char* group = r_grpname(arg);
+  if(!group)
+  {
+    printf("Content-type: text/plain\r\n\r\nERROR: unknown group alias: %s\n", arg);
+    return;
+  }
   const char* cc1 =
   "Content-type: text/html\r\n\r\n"
   "<!DOCTYPE html>\r\n<html>\r\n"
@@ -25,11 +54,11 @@ static void r_form(char* arg)
   "<body>\r\n<h2>Weekly Report (Week %d)</h2>\r\n<h3>%s</h3>"
   "<form action=/cgi-bin/report?submit&%s method=post enctype=text/plain>\r\n"
   "Name: <input name=name type=text size=32></input>\r\n<ol>\r\n";
-  printf(cc1, r_weeknum(), arg, arg);
-  int i = 10;
+  printf(cc1, r_weeknum(), group, arg);
+  int i = 16;
   while(i--) puts("<li><input name=task type=text size=64></input></li>");
-  printf("</ol>\r\n<h2>Next Week (Week%d):</h2>\r\n<input name=ww type=hidden value=---------></input>\r\n<ol>\r\n", r_weeknum() + 1);
-  i = 10;
+  printf("</ol>\r\n<h2>Next Week (Week %d):</h2>\r\n<input name=ww type=hidden value=---------></input>\r\n<ol>\r\n", r_weeknum() + 1);
+  i = 16;
   while(i--) puts("<li><input name=task type=text size=64></input></li>");
   puts("</ol>\r\n<input type=submit value=Submit></input>\r\n</form>\r\n</body>\r\n</html>");
 }
@@ -55,28 +84,20 @@ static char* r_line()
   return value;
 }
 
-static void r_report()
+static void r_report(const char* gr, const char* gname)
 {
   char buff[4096];
   sprintf(buff, "../%s.html", r_weekstr());
   FILE* rf = fopen(buff, "w");
   if(!rf) return;
-  fprintf(rf, "<html><body>\r\n");
+  fprintf(rf, "<html><head><meta http-equiv=\"pragma\" content=\"no-cache\" /></head><body>\r\n<h2><u>%s</u></h2>\r\n", gname);
   struct dirent *de = 0;
   DIR* pd = opendir(".");
-  int gc = 0;
   while((de = readdir(pd)))
   {
     if(de->d_type != DT_REG) continue;
     FILE* ff = fopen(de->d_name, "r");
-    char* gr = fgets(buff, sizeof(buff), ff);//group
-    if(!gc)
-    {
-      fprintf(rf, "<h2><u>%s</u></h2>\r\n", gr);
-      printf("<a href=\"/reports/%s/%s.html\">Full Report</a>\r\n</body></html>", gr, r_weekstr());
-      gc ++;
-    }
-    fprintf(rf, "<h2>%s</h2>\r\n", fgets(buff, sizeof(buff), ff));//name
+    fprintf(rf, "<h2>%s</h2>\r\n", de->d_name);//name
     fprintf(rf, "<ol>\r\n");
     while(fgets(buff, sizeof(buff), ff) && strcmp(buff, "---------\r\n")) fprintf(rf, "<li>%s</li>\r\n", buff);
     fprintf(rf, "</ol>\r\nWW%d tasks:<ol>\r\n", r_weeknum() + 1);
@@ -87,33 +108,44 @@ static void r_report()
   closedir(pd);
   fprintf(rf, "</body></html>\r\n");
   fclose(rf);
+  sprintf(buff, "abiword ../%s.html -t doc", r_weekstr());
+  printf("<a href=\"/reports/%s/%s.html\">View online</a><br>\r\n", gr, r_weekstr());
+  if(system(buff));
+  printf("<a href=\"/reports/%s/%s.doc\">Download doc file</a>\r\n</body></html>", gr, r_weekstr());
 }
 
-static void r_submit(char* group)
+static void r_submit(char* gr)
 {
-  if(!*group) return;
+  if(!*gr) return;
+  const char* group = r_grpname(gr);
+  if(!group)
+  {
+    printf("Content-type: text/plain\r\n\r\nERROR: unknown group alias: %s\n", gr);
+    return;
+  }
   if(chdir("../reports")) return;
-  mkdir(group, 0755);
-  if(chdir(group)) return;
+  mkdir(gr, 0755);
+  if(chdir(gr)) return;
   char* week = r_weekstr();
   mkdir(week, 0755);
   if(chdir(week)) return;
   char* line = r_line();
   if(!line) return;
   FILE* ff = fopen(line, "w");
-  if(!ff) return;
+  if(!ff)
+  {
+    return;
+  }
   const char* cc1 = 
   "Content-type: text/html\r\n\r\n<html><body>Submited.<hr>\r\nUser: %s<br>\r\nGroup: %s<br>\r\n";
   printf(cc1, line, group);
-  fprintf(ff, "%s\r\n", group);
-  fprintf(ff, "%s\r\n", line);
   while((line = r_line())) {
     if(!*line) continue;
     fprintf(ff, "%s\r\n", line);
   }
   fclose(ff);
   printf("<hr>\r\n");
-  r_report();
+  r_report(gr, group);
 }
 
 int main(int argc, char* argv[])
