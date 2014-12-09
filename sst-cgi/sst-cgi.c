8e9e368697674d98a86b2fada0d0f2822b88c3de
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2014-12-09 16:23:48 +0400

    fixed fgets return warning

--- sst-cgi/sst-cgi.c
+++ sst-cgi/sst-cgi.c
@@ -5,13 +5,15 @@
 #define MAX_CMD_LENGTH 0x100
 
 
-static void cmd_unknown() {
+static void cmd_unknown()
+{
 	puts("Content-type: text/plain; charset=UTF-8\n\n");
 	puts("ERR");
 	puts("Unknown command");
 }
 
-static void cmd_echo_line() {
+static void cmd_echo_line()
+{
 	char buff[PAGE_SIZE];
 	if(fgets(buff, sizeof(buff), stdin)) {
 		puts("Content-type: text/plain; charset=UTF-8\n\n");
@@ -20,10 +22,10 @@ static void cmd_echo_line() {
 	}
 }
 
-static void* dispatch_cmd() {
+static void* dispatch_cmd()
+{
 	char cmd_buff[MAX_CMD_LENGTH];
-	*cmd_buff = 0;
-	fgets(cmd_buff, sizeof(cmd_buff), stdin);
+	if(!fgets(cmd_buff, sizeof(cmd_buff), stdin)) return cmd_unknown;
 	if(!strcmp(cmd_buff, "echo-line\n")) return cmd_echo_line;
 	return cmd_unknown;
 }
