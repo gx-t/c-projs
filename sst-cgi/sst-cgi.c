74538fc836a9f30165c3e77baac406d81d1a3e11
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2014-12-09 23:27:52 +0400

    Added uuidgen support using child process

--- sst-cgi/sst-cgi.c
+++ sst-cgi/sst-cgi.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-#include <uuid/uuid.h>
-#include <unistd.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <sys/types.h>
+#ifndef NO_UUID_LIB
+#include <uuid/uuid.h>
+#endif //NO_UUID_LIB
 
 #define PAGE_SIZE			0x1000
 #define MAX_CMD_LENGTH			0x100
 #define MIN_PASSWORD_LENGTH		5
-static const char users_root[] =	"users/";
 #define UUID_SIZE			36
+static const char users_root[]     =	"users/";
+static const char user_dir_inbox[] =	"inbox/";
 
 static void resp_text_ok()
 {
@@ -43,20 +46,40 @@ static int get_cllient_line(char* buff, size_t buff_size, const char* field_name
 	if(!fgets(buff, buff_size, stdin) || strlen(buff) < min_length) {
 		resp_text_err();	
 		printf("You must specify at least %lu character long %s\n", min_length, field_name);
-		return 0;
+		return 1;
 	}
-	return 1;
+	return 0;
+}
+
+static void gen_uuid(char* buff)
+{
+#ifdef NO_UUID_LIB
+	FILE* pf = popen("uuidgen", "r");
+	if(!pf) {
+		perror("uuidgen");
+		return;
+	}
+	fread(buff, UUID_SIZE, 1, pf);
+	pclose(pf);
+	buff[UUID_SIZE] = 0;
+#else
+	uuid_t uu;
+	uuid_generate_time(uu);
+	uuid_unparse(uu, buff);
+#endif //NO_UUID_LIB
 }
 
 static char* db_create_enter_new_user_dir(char* buff)
 {
-	uuid_t uu;
-	memset(buff, 0, sizeof(users_root) + UUID_SIZE);
-	uuid_generate_time(uu);
 	strcpy(buff, users_root);
-	uuid_unparse(uu, buff + sizeof(users_root) - 1);
+	gen_uuid(buff + sizeof(users_root) - 1);
 	if(mkdir(buff, 0700) || chdir(buff)) perror(buff);
 	return buff + sizeof(users_root) - 1;
+}
+
+static void db_make_user_dirs()
+{
+	if(mkdir(user_dir_inbox, 0700)) perror(user_dir_inbox);
 }
 
 static void cmd_register()
@@ -64,6 +87,7 @@ static void cmd_register()
 	char buff[PAGE_SIZE];
 	if(!get_cllient_line(buff, sizeof(buff), "password", MIN_PASSWORD_LENGTH)) {
 		char* uuid_str = db_create_enter_new_user_dir(buff);
+		db_make_user_dirs();
 		resp_text_ok();	
 		puts(uuid_str);
 	}
