e366ac99a63bce2de3962c05ef06d8c2ae815a6a
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2015-03-31 00:27:40 +0400

    Test 1

--- at91sam9g20-test/at91sam9g20.h
+++
@@ -1,8 +0,0 @@
-#ifndef __AT91SAM9G20_H__
-#define __AT91SAM9G20_H_
-
-#define PIOB_BASE 0xfffff600UL
-#define PIOB_SODR (PIOB_BASE + 0x0030)
-#define PIOB_CODR (PIOB_BASE + 0x0034)
-
-#endif //__AT91SAM9G20_H_
