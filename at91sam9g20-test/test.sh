44ecda3128459aaa7fc6b8246fcf591f66bb4899
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2015-04-27 18:52:57 +0400

    Added context to test script, added parameters

--- at91sam9g20-test/test.sh
+++ at91sam9g20-test/test.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-echo ds18b20 4 presense
+LED="board0.led0"
+TERM="board0.term0"
+PERIOD=1
+echo ds18b20 $TERM 4 presense
 while true
 do
-	echo gpio board0.led0 7 1
-	echo ds18b20 board0.term0 4 convert
-	sleep 1
-	echo gpio board0.led0 7 0
-	echo ds18b20 board0.term0 4 read
-	sleep 1
+	echo gpio $LED 7 1
+	echo ds18b20 $TERM 4 convert
+	sleep $PERIOD
+	echo gpio $LED 7 0
+	echo ds18b20 $TERM 4 read
+	sleep $PERIOD
 done | ./test shell
 
