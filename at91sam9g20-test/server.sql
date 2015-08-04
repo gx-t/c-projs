d29bb5bcb03173a8e18ea8eb342f0626b29f62e4
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2015-08-04 23:17:08 +0400

    Added 'type' column to keys table to support 'group' type

--- at91sam9g20-test/server.sql
+++ at91sam9g20-test/server.sql
@@ -1,5 +1,5 @@
---Table templates
-CREATE TABLE "keys" (key text, name text, descr text, status text, time timestamp, parent text, unique(key) on conflict abort);
+---Table templates
+CREATE TABLE "keys" (key text, name text, descr text, status text, time timestamp, parent text, type text, unique(key) on conflict abort);
 CREATE TABLE "22265df2-1e31-4f8f-a778-53d724c8ff9c.config" (name text, value text, unique(name) on conflict replace);
 CREATE TABLE "22265df2-1e31-4f8f-a778-53d724c8ff9c.data" (time TIMESTAMP, devid text, value real, key text);
 CREATE TABLE "c893b811-352e-4667-b740-0d6ed91d0f95.config" (name text, value text, unique(name) on conflict replace);
