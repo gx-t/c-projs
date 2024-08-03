378f1494bdb069a38f8011554c02cf343f5cb3cd
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2024-08-04 00:22:32 +0400

    Using structure for protocol

--- udp-file/udp-file.c
+++ udp-file/udp-file.c
@@ -13,7 +13,6 @@
 #include <openssl/sha.h>
 
 #define CHUNK_SIZE              1024
-#define CHUNK_HEADER_SIZE       48
 
 static int __argc__;
 static char** __argv__;
@@ -23,17 +22,17 @@ static int running = 1;
 enum
 {
     ERR_OK
-    , ERR_ARGC
-    , ERR_SUBCMD
-    , ERR_ARGV
-    , ERR_FILE
-    , ERR_ALGO
+        , ERR_ARGC
+        , ERR_SUBCMD
+        , ERR_ARGV
+        , ERR_FILE
+        , ERR_ALGO
 };
 
 enum
 {
     CMD_SEND_FILE_CHUNK = 2
-    , CMD_DEL_FILE_CHUNK
+        , CMD_DEL_FILE_CHUNK
 };
 
 static int show_usage(int err, const char* descr)
@@ -46,62 +45,50 @@ static int show_usage(int err, const char* descr)
     return err;
 }
 
-// --- CHUNK-ENCRYPTED FILE FORMAT
-// flag - 1 byte, acknoledge received
-// number of attempts to send - 1 byte
-
-// --- UDP FILE CHUNK FORMAT (16 byte padding)
-// random - 16 bytes iv for AES
-// command protocol command meaning "send file chunk" - 1 bytes
-// padding - 9 bytes
-// file name zero terminated string - 32 bytes
-// chunk number - 2 bytes, network byte order
-// total chunks - 2 bytes, network byte order
-// data length - 2 byte
-// data - 1024 bytes (with padding to 1024)
-// hash - 32 bytes
-
-static void encrypt_chunk(uint8_t mk[2 * AES_BLOCK_SIZE]
-        , const char* file_name
-        , uint16_t chunk_num
-        , uint16_t chunk_count
-        , uint16_t chunk_len
-        , uint8_t chunk_buff[CHUNK_HEADER_SIZE + CHUNK_SIZE])
+struct UDP_FILE_CHUNK
 {
-    uint8_t iv[AES_BLOCK_SIZE];
-    RAND_bytes(iv, AES_BLOCK_SIZE);
+    struct
+    {
+        uint8_t iv[16];
+        struct
+        {
+            uint8_t cmd;
+            uint8_t padding[9];
+            char file_name[32];
+            uint16_t chunk_num;
+            uint16_t chunk_count;
+            uint16_t data_len;
+            uint8_t data[CHUNK_SIZE];
+        } enc_data;
+    } hashed_data;
+    uint8_t hash[32];
+};
 
-// file chunk header
-    putchar(0); //flag
-    putchar(0); //number of attempts to send
+struct FILE_CHUNK
+{
+    uint8_t flag;
+    uint8_t send_count;
+    struct UDP_FILE_CHUNK udp_chunk;
+};
 
-// UDP data header
-    fwrite(iv, sizeof(iv), 1, stdout);
-
-    uint8_t* pp = chunk_buff;
-
-    *pp ++ = CMD_SEND_FILE_CHUNK;
-    pp += 9; //padding
-    memcpy(pp, file_name, 32);
-    pp += 32;
-    *((uint16_t*)pp) = htons(chunk_num);
-    pp += sizeof(uint16_t);
-    *((uint16_t*)pp) = htons(chunk_count);
-    pp += sizeof(uint16_t);
-    *((uint16_t*)pp) = htons(chunk_len);
-    pp += sizeof(uint16_t);
+static void encrypt_chunk(uint8_t mk[2 * AES_BLOCK_SIZE], struct UDP_FILE_CHUNK* chunk)
+{
+    RAND_bytes(chunk->hashed_data.iv, AES_BLOCK_SIZE);
 
     AES_KEY key;
     AES_set_encrypt_key(mk, 256, &key);
-    AES_cbc_encrypt(chunk_buff, chunk_buff, CHUNK_HEADER_SIZE + CHUNK_SIZE, &key, iv, AES_ENCRYPT);
-    fwrite(chunk_buff, CHUNK_HEADER_SIZE + CHUNK_SIZE, 1, stdout);
 
-    uint8_t hash[32];
+    AES_cbc_encrypt((uint8_t*)&chunk->hashed_data.enc_data
+            , (uint8_t*)&chunk->hashed_data.enc_data
+            , sizeof(chunk->hashed_data.enc_data)
+            , &key
+            , chunk->hashed_data.iv
+            , AES_ENCRYPT);
+
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
-    SHA256_Update(&sha256, chunk_buff, CHUNK_HEADER_SIZE + CHUNK_SIZE);
-    SHA256_Final(hash, &sha256);
-    fwrite(hash, 32, 1, stdout);
+    SHA256_Update(&sha256, &chunk->hashed_data, sizeof(chunk->hashed_data));
+    SHA256_Final(chunk->hash, &sha256);
 }
 
 static int enc_main()
@@ -141,13 +128,26 @@ static int enc_main()
         uint16_t chunk_num = 0;
         while(running && !feof(stdin))
         {
-            uint8_t chunk_buff[CHUNK_HEADER_SIZE + CHUNK_SIZE];
-            memset(chunk_buff, 0x55, sizeof(chunk_buff));
-            size_t chunk_len = fread(chunk_buff + CHUNK_HEADER_SIZE, 1, CHUNK_SIZE, stdin); 
-            if(chunk_len != CHUNK_SIZE)
-                fprintf(stderr, "===>>> Last chunk length is %lu\n", chunk_len);
+            struct FILE_CHUNK chunk_buff = {.flag = 0, .send_count = 0, /*.padding = {0, 0}*/};
+            memset(&chunk_buff.udp_chunk, 0x55, sizeof(chunk_buff.udp_chunk));
+            chunk_buff.udp_chunk.hashed_data.enc_data.cmd = CMD_SEND_FILE_CHUNK;
+            strcpy(chunk_buff.udp_chunk.hashed_data.enc_data.file_name, file_name);
+            chunk_buff.udp_chunk.hashed_data.enc_data.chunk_num = htons(chunk_num);
+            chunk_buff.udp_chunk.hashed_data.enc_data.chunk_count = htons(chunk_count);
 
-            encrypt_chunk(mk, file_name, chunk_num, chunk_count, chunk_len, chunk_buff);
+            long data_len = fread(chunk_buff.udp_chunk.hashed_data.enc_data.data
+                    , 1
+                    , CHUNK_SIZE
+                    , stdin); 
+
+            if(data_len != CHUNK_SIZE)
+                fprintf(stderr, "===>>> Last chunk length is %lu\n", data_len);
+
+            chunk_buff.udp_chunk.hashed_data.enc_data.data_len = htons((uint16_t)data_len);
+
+            encrypt_chunk(mk, &chunk_buff.udp_chunk);
+            fwrite(&chunk_buff, sizeof(chunk_buff), 1, stdout);
+
             chunk_num ++;
         }
         if(chunk_num != chunk_count)
@@ -208,3 +208,4 @@ int main(int argc, char* argv[])
 
     return show_usage(ERR_SUBCMD, "Unknown subcommand");
 }
+// 2443716 bytes
