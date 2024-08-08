082bc98e8dfbc378edba7ec607f29662e42233fb
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2024-08-08 21:27:20 +0400

    Added test file and test script

---
+++ udp-file/run-test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+key="00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF"
+INBOX="/tmp/inbox"
+OUTBOX="/tmp/outbox"
+
+mkdir -p $INBOX
+mkdir -p $OUTBOX
+rm -rf $INBOX/*
+rm -rf $OUTBOX/*
+
+./udp-file enc $key img1.jpeg < test-data/IMG_20240101_160447_HDR.jpg > $INBOX/test1.bin &&
+./udp-file enc $key img2.mp4 < test-data/IMG_1095-1.MP4 > $INBOX/test2.bin &&
+
+cat $INBOX/test1.bin $INBOX/test2.bin > $INBOX/test.bin &&
+
+ls -lh $INBOX &&
+
+rm $INBOX/test1.bin $INBOX/test2.bin &&
+
+
+ls -lh $INBOX &&
+
+./udp-file shuffle $INBOX/test.bin &&
+
+# ./udp-file dump $INBOX/test.bin
+
+./udp-file dec $key $OUTBOX < $INBOX/test.bin &&
+
+ls -lh $OUTBOX &&
+
+diff test-data/IMG_20240101_160447_HDR.jpg $OUTBOX/img1.jpeg &&
+diff test-data/IMG_1095-1.MP4 $OUTBOX/img2.mp4 &&
+
+echo PASSED
