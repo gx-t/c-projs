4fd0b9e8503d0e3e267679fad5b60c2c61de1b0b
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2025-11-27 20:25:12 +0400

    Move to SDL3

--- sdl-test/00-rand-dots.c
+++ sdl-test/00-rand-dots.c
@@ -1,13 +1,13 @@
-#include <SDL.h>
-#include <SDL_ttf.h>
+#include <SDL3/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static int running = 1;
 
 int main(int argc, char *argv[])
 {
     int res = 0;
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    if(!SDL_Init(SDL_INIT_VIDEO))
     {
         fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
         return 1;
@@ -15,83 +15,47 @@ int main(int argc, char *argv[])
 
     SDL_Window* win = 0;
     SDL_Renderer* rend = 0;
-    TTF_Font* font = 0;
     SDL_Texture* texture = 0;
 
     do {
-        if(TTF_Init() != 0)
-        {
-            fprintf(stderr, "TTF_Init Error: %s\n", TTF_GetError());
-            res = 1;
-            break;
-        }
-
         if(!(win = SDL_CreateWindow("SDL test"
-                        , SDL_WINDOWPOS_CENTERED
-                        , SDL_WINDOWPOS_CENTERED
                         , 640
                         , 480
-                        , SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE)))
+                        , SDL_WINDOW_RESIZABLE)))
         {
             fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
             res = 2;
             break;
         }
+        SDL_SetWindowPosition(win, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 
-        if(!(rend = SDL_CreateRenderer(win
-                        , -1
-                        , SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)))
+        if(!(rend = SDL_CreateRenderer(win, NULL)))
         {
             fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
             res = 3;
             break;
         }
-
-        if(!(font = TTF_OpenFont(TTF_PATH, 24)))
-        {
-            fprintf(stderr, "TTF_OpenFont Error: %s\n", TTF_GetError());
-            res = 4;
-            break;
-        }
-
-//        SDL_Color color = {255, 0, 0, 128};
-//        SDL_Surface* surface = TTF_RenderText_Solid(font, "SDL Test", color);
-//        if(!surface)
-//        {
-//            fprintf(stderr, "TTF_RenderText_Solid Error: %s\n", TTF_GetError());
-//            res = 5;
-//            break;
-//        }
-//
-//        texture = SDL_CreateTextureFromSurface(rend, surface);
-//        SDL_FreeSurface(surface);
+        SDL_SetRenderVSync(rend, 1);
         if(!(texture = SDL_CreateTexture(rend
-                , SDL_PIXELFORMAT_RGBA8888
-                , SDL_TEXTUREACCESS_TARGET
-                , 64
-                , 48)))
+                        , SDL_PIXELFORMAT_RGBA8888
+                        , SDL_TEXTUREACCESS_TARGET
+                        , 64
+                        , 48)))
         {
             fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
             res = 6;
             break;
         }
 
-//        SDL_Rect dst;
-//        dst.x = 100;
-//        dst.y = 100;
-//        TTF_SizeText(font, "   ", &dst.w, &dst.h);
-
-
         while(running)
         {
             SDL_Event e;
             while(SDL_PollEvent(&e))
             {
-                if(SDL_QUIT == e.type)
+                if(SDL_EVENT_QUIT == e.type)
                     running = 0;
             }
 
-//            SDL_Point pointArr[8] = {0};
             int count = 8;
             const SDL_Color colorArr[] =
             {
@@ -104,35 +68,30 @@ int main(int argc, char *argv[])
                 , {0xFF, 0xFF, 0xFF, 0xFF}
             };
             SDL_SetRenderTarget(rend, texture);
-            SDL_SetRenderDrawColor(rend, 0x00, 0x00, 0x00, 0x00);
+            SDL_SetRenderDrawColor(rend, 0x00, 0x00, 0x00, 0xFF);
             SDL_RenderClear(rend);
-            SDL_RenderSetScale(rend, 1, 1);
+            SDL_SetRenderScale(rend, 1.0, 1.0);
             while(count --)
             {
-//                pointArr[count].x = rand() % 16;
-//                pointArr[count].y = rand() % 16;
                 const SDL_Color* clr = &colorArr[rand() % sizeof(colorArr) / sizeof(colorArr[0])];
                 SDL_SetRenderDrawColor(rend, clr->r, clr->g, clr->b, clr->a);
-                SDL_RenderDrawPoint(rend, rand() % 64, rand() % 48);
+                SDL_RenderPoint(rend, rand() % 64, rand() % 48);
             }
 
-//            SDL_SetRenderDrawColor(rend, 0xFF, 0xFF, 0xFF, 0xFF);
-//            SDL_RenderDrawPoints(rend, pointArr, sizeof(pointArr) / sizeof(pointArr[0]));
+            //            SDL_RenderDrawPoints(rend, pointArr, sizeof(pointArr) / sizeof(pointArr[0]));
             SDL_SetRenderTarget(rend, NULL);
 
             SDL_RenderClear(rend);
-            SDL_RenderCopy(rend, texture, NULL, NULL);
+            SDL_RenderTexture(rend, texture, NULL, NULL);
             SDL_RenderPresent(rend);
             SDL_Delay(50);
         }
     } while(0);
 
     SDL_DestroyTexture(texture);
-    TTF_CloseFont(font);
     SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
-    TTF_Quit();
     SDL_Quit();
-    
+
     return res;
 }
--- sdl-test/01-puzzle15.c
+++ sdl-test/01-puzzle15.c
@@ -1,10 +1,10 @@
-#include <SDL.h>
-#include <SDL_ttf.h>
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-static SDL_bool running = SDL_TRUE;
+static bool running = true;
 
 const static int cellWidth = 50;
 static int board[4][4] =
@@ -18,7 +18,7 @@ static int board[4][4] =
 static int x_empty = 3;
 static int y_empty = 3;
 
-static void tileNumToRect(SDL_Rect* rc, int x, int y)
+static void tileNumToRect(SDL_FRect* rc, int x, int y)
 {
     rc->x = x * cellWidth;
     rc->y = y * cellWidth;
@@ -31,7 +31,7 @@ static void prepareSprite(SDL_Renderer* rend, SDL_Texture* sprite)
     TTF_Font* font = TTF_OpenFont(TTF_PATH, cellWidth / 2);
     if(!font)
     {
-        fprintf(stderr, "TTF_OpenFont Error: %s\n", TTF_GetError());
+        fprintf(stderr, "TTF_OpenFont Error: %s\n", SDL_GetError());
         return;
     }
     const SDL_Color numColor = {0xFF, 0xFF, 0xFF, 0xFF};
@@ -40,12 +40,12 @@ static void prepareSprite(SDL_Renderer* rend, SDL_Texture* sprite)
     {
         for(int x = 0; x < 4; x ++)
         {
-            SDL_Rect rc;
+            SDL_FRect rc;
             tileNumToRect(&rc, x, y);
 
             SDL_SetRenderDrawColor(rend, 0, 0xFF, 0x00, 0xFF);
             SDL_SetRenderTarget(rend, sprite);
-            SDL_RenderDrawRect(rend, &rc);
+            SDL_RenderRect(rend, &rc);
 
             int num = x + 4 * y;
             if(!num)
@@ -55,15 +55,15 @@ static void prepareSprite(SDL_Renderer* rend, SDL_Texture* sprite)
 
             if(num)
                 sprintf(str, "%d", num);
-            SDL_Surface* surface = TTF_RenderText_Blended(font, str, numColor);
+            SDL_Surface* surface = TTF_RenderText_Blended(font, str, 0, numColor);
             SDL_Texture* tmpText = SDL_CreateTextureFromSurface(rend, surface);
-            SDL_FreeSurface(surface);
+            SDL_DestroySurface(surface);
 
-            SDL_QueryTexture(tmpText, NULL, NULL, &rc.w, &rc.h);
+            SDL_GetTextureSize(tmpText, &rc.w, &rc.h);
             rc.x += (cellWidth - rc.w) / 2;
             rc.y += (cellWidth - rc.h) / 2;
             SDL_SetRenderTarget(rend, sprite);
-            SDL_RenderCopy(rend, tmpText, NULL, &rc);
+            SDL_RenderTexture(rend, tmpText, NULL, &rc);
             SDL_DestroyTexture(tmpText);
         }
     }
@@ -97,17 +97,6 @@ static void swapTile(SDL_Renderer* rend, SDL_Texture* sprite, int x, int y)
     }
 }
 
-static void handleLeftClick(SDL_Renderer* rend, SDL_Texture* sprite, int x, int y)
-{
-    if(0 > x || 0 > y || 4 * cellWidth < x || 4 * cellWidth < y)
-        return;
-
-    x /= cellWidth;
-    y /= cellWidth;
-
-    swapTile(rend, sprite, x, y);
-}
-
 static void shuffle(SDL_Renderer* rend, SDL_Texture* sprite)
 {
     for(int i = 0; i < 10000; i ++)
@@ -123,83 +112,28 @@ static void drawBoard(SDL_Renderer* rend, SDL_Texture* sprite)
     {
         for(int x = 0; x < 4; x ++)
         {
-            SDL_Rect src, dst;
+            SDL_FRect src, dst;
             tileNumToRect(&src, board[y][x] % 4, board[y][x] / 4);
             tileNumToRect(&dst, x, y);
-            SDL_RenderCopy(rend, sprite, &src, &dst);
+            SDL_RenderTexture(rend, sprite, &src, &dst);
         }
     }
     SDL_RenderPresent(rend);
 }
 
-static void shuffleLoop(SDL_Renderer* rend, SDL_Texture* sprite)
-{
-    while(running)
-    {
-        SDL_Event evt;
-        while(SDL_PollEvent(&evt))
-        {
-            if(SDL_QUIT == evt.type)
-            {
-                running = SDL_FALSE;
-                return;
-            }
-            if(SDL_MOUSEBUTTONDOWN == evt.type
-                    && (SDL_BUTTON_LEFT == evt.button.button
-                        || SDL_BUTTON_RIGHT == evt.button.button))
-                return;
-        }
-        drawBoard(rend, sprite);
-        shuffle(rend, sprite);
-    }
-}
-
-static void mainScreenEventLoop(SDL_Renderer* rend, SDL_Texture* sprite)
-{
-    SDL_Event evt;
-    while(running && SDL_WaitEvent(&evt))
-    {
-        switch(evt.type)
-        {
-            case SDL_QUIT:
-                running = SDL_FALSE;
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-                switch(evt.button.button)
-                {
-                    case SDL_BUTTON_LEFT:
-                        handleLeftClick(rend, sprite, evt.button.x, evt.button.y);
-                        break;
-                    case SDL_BUTTON_RIGHT:
-                        shuffleLoop(rend, sprite);
-                        break;
-                }
-                drawBoard(rend, sprite);
-                break;
-            case SDL_WINDOWEVENT:
-                if(SDL_WINDOWEVENT_EXPOSED == evt.window.event)
-                    drawBoard(rend, sprite);
-                break;
-        }
-    }
-}
-
 int main()
 {
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+    SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
     SDL_Window* win = SDL_CreateWindow("Puzzle-15"
-            , SDL_WINDOWPOS_CENTERED
-            , SDL_WINDOWPOS_CENTERED
             , 400
             , 400
-            , SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+            , SDL_WINDOW_RESIZABLE);
+    SDL_SetWindowPosition(win, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 
-    SDL_Renderer* rend = SDL_CreateRenderer(win
-            , -1
-            , SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-    SDL_RenderSetLogicalSize(rend, cellWidth * 4, cellWidth * 4);
+    SDL_Renderer* rend = SDL_CreateRenderer(win, NULL);
+    SDL_SetRenderVSync(rend, 1);
+    SDL_SetRenderLogicalPresentation(rend, cellWidth * 4, cellWidth * 4, SDL_LOGICAL_PRESENTATION_STRETCH);
 
     SDL_Texture* sprite = SDL_CreateTexture(rend
             , SDL_PIXELFORMAT_RGBA8888
@@ -211,7 +145,55 @@ int main()
 
     srand(time(NULL));
 
-    mainScreenEventLoop(rend, sprite);
+    SDL_Event evt;
+    while(running && SDL_WaitEvent(&evt))
+    {
+        switch(evt.type)
+        {
+            case SDL_EVENT_QUIT:
+                running = false;
+                break;
+            case SDL_EVENT_MOUSE_BUTTON_DOWN:
+                switch(evt.button.button)
+                {
+                    case SDL_BUTTON_LEFT:
+                        {
+                            int w, h;
+                            SDL_GetWindowSize(win, &w, &h);
+                            swapTile(rend
+                                    , sprite
+                                    , (int)(4.0 * evt.button.x / w)
+                                    , (int)(4.0 * evt.button.y / h));
+                        }
+                        break;
+                    case SDL_BUTTON_RIGHT:
+                        while(running)
+                        {
+                            while(SDL_PollEvent(&evt))
+                            {
+                                if(SDL_EVENT_QUIT == evt.type)
+                                {
+                                    running = false;
+                                    goto stop_shuffle;
+                                }
+                                if(SDL_EVENT_MOUSE_BUTTON_DOWN == evt.type
+                                        && (SDL_BUTTON_LEFT == evt.button.button
+                                            || SDL_BUTTON_RIGHT == evt.button.button))
+                                    goto stop_shuffle;
+                            }
+                            drawBoard(rend, sprite);
+                            shuffle(rend, sprite);
+                        }
+stop_shuffle:
+                        break;
+                }
+                drawBoard(rend, sprite);
+                break;
+            case SDL_EVENT_WINDOW_EXPOSED:
+                drawBoard(rend, sprite);
+                break;
+        }
+    }
 
     SDL_DestroyTexture(sprite);
     SDL_DestroyRenderer(rend);
